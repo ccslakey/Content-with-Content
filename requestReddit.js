3a11b555a637b628a30fda9f3d3dd737872d6e3d
ccslakey <cromwellslakey@gmail.com>
2015-10-22 14:18:02 -0700

    event listeners and more functions

--- requestReddit.js
+++ requestReddit.js
@@ -3,33 +3,23 @@ $(function(){
 	var ajaxButton = $('.ajaxButton');
 	var contentList = $('#contentList');
 	var redditData = [];
+	var renderThreads = $('.renderThread')
 
-	function makeCard (post) {
-		// body...
-	}
-
-	function getThumbnails (val, index, dataChildren) {
-		post = (val.data);
-		var htmlCard = "<li><img alt='" + post.url + "' src='" + post.thumbnail + "'><br>" + post.title + "</li>"; 
-
-
-		// change if statement to accept nsfw?
-		// just find different json link
-		if (post.thumbnail !="nsfw"){
-			contentList.append(htmlCard);
-		}
-
+	function renderThread () {
+		var thread = "<p>Hi</p>";
+		return thread
 	}
 
 
-
-	// event listener to load some data
+	// event listener to load some subreddit data
 	ajaxButton.click(function(e){
-		contentList.html('');
-		var subreddit = $("#subreddit")[0].value || "pics"
-
 		e.preventDefault();
-		$.getJSON("http://www.reddit.com/r/" + subreddit + ".json?limit=100&jsonp=?", function (data) {
+		// reset content
+		contentList.empty();
+		// get subreddit and make ajax call
+		var subreddit = $("#subreddit")[0].value || "pics";
+		// ajax
+		$.getJSON("http://www.reddit.com/r/" + subreddit + ".json?limit=10&jsonp=?", function (data) {
 	            redditData = data.data.children;
 	            
 	            // scrape the mods
@@ -40,10 +30,30 @@ $(function(){
 	            // gimme that sweet content
 	            redditData.forEach(getThumbnails);
 
-
 	           }); //end of ajax call
 		}); //end .click
 
+	function makeCard (post) {
+		var constructedCard = '<div class="mdl-layout mdl-js-layout mdl-color--grey-100"><main class="mdl-layout__content"><div class="mdl-grid"><div class="mdl-card mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-shadow--2dp"><figure class="mdl-card__media">'+
+		'<img src="'+ post.url + '" alt="Image Alt" />'+ '</figure><div class="mdl-card__title"><h1 class="mdl-card__title-text">' + 
+			'/u/' + post.author + '</h1></div><div class="mdl-card__supporting-text"><p>' +
+			post.title + 
+			'</p></div><div class="mdl-card__actions mdl-card--border"><a class="renderThread mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Read More</a>'+
+			'</div></div></div></main></div>'
+		return constructedCard;    
+	}
+
+	function getThumbnails (val, index, post) {
+		post = (val.data);
+		var htmlCard = makeCard(post)
+		
+		// change if statement to accept nsfw?
+		// just find different json link
+		if (post.thumbnail !="nsfw"){
+			contentList.append(htmlCard);
+		}
+
+	}
 
 
 	
